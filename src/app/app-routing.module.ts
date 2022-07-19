9d2c67fe57509c835d8e7ae23d1cf634b1c6bb91
JaoMoreti <joao.moreti@ekaizen.digital>
2022-07-19 11:11:23 -0300

    commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarPessoaComponent } from './pessoa/editar-pessoa/editar-pessoa.component';
+import { InserirPessoaComponent } from './pessoa/inserir-pessoa/inserir-pessoa.component';
 
-const routes: Routes = [];
+import { ListarPessoaComponent } from './pessoa/listar-pessoa/listar-pessoa.component';
+
+const routes: Routes = [
+  { path: '',
+  redirectTo: 'pessoas/listar',
+  pathMatch: 'full' },
+{   path: 'pessoas',
+  redirectTo: 'pessoas/listar' },
+{   path: 'pessoas/listar',
+  component: ListarPessoaComponent },
+  { path: 'pessoas/novo',
+    component: InserirPessoaComponent},
+    { path: 'pessoas/editar/:id',
+      component: EditarPessoaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
